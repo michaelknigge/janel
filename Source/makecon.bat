895f1e3248a45eafc1b898f4f2e2d78ed9fe7c01
Michael Knigge <michael.knigge@gmx.de>
2017-09-04 14:38:21 +0200

    The build process was simplified. If the environment variable JAVA_HOME is set and points to a JDK, Janel can be built without modifying the Visual Studio project files.

--- Source/makecon.bat
+++ Source/makecon.bat
@@ -1,8 +1,8 @@
 @echo off
 @setlocal
 
-rem Change the INCLUDE environment variable to the appropriate directories on your machine
-rem set INCLUDE=%INCLUDE%;C:\Java\j2sdk1.4.2_15\include\;C:\Java\j2sdk1.4.2_15\include\win32\
+rem Change the JAVA_HOME environment variable to the directory of your JDK (*NOT* JRE).
+rem set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_101
 
 echo Making the console executable
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.com" Janel.sln /rebuild "Release Console|Win32"
