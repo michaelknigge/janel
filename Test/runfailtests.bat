f6b8e287203d93a393baab90f0b952098dd902da
Michael Knigge <michael.knigge@gmx.de>
2017-09-05 22:03:16 +0200

    Unit-Tests that may display a MessageBox or require elevated rights can now be skipped. This is useful if you want to run them unattended on a server.

--- Test/runfailtests.bat
+++ Test/runfailtests.bat
@@ -5,7 +5,8 @@ SETLOCAL
 if "%ANT_HOME%"=="" set ANT_HOME=C:\Programs\apache-ant-1.6.1
 if "%JAVA_HOME%"=="" set JAVA_HOME=C:\Java\j2sdk1.4.2_05
 
-call %ANT_HOME%\bin\ant.bat -f AntTestFail.xml
+REM Set "headless=true" to disable all GUI tests
+call %ANT_HOME%\bin\ant.bat -f AntTestFail.xml -Dheadless=false
 
 PAUSE
 
--- Test/runtests.bat
+++ Test/runtests.bat
@@ -5,7 +5,9 @@ SETLOCAL
 if "%ANT_HOME%"=="" set ANT_HOME=C:\Programs\apache-ant-1.6.1
 if "%JAVA_HOME%"=="" set JAVA_HOME=C:\Java\j2sdk1.4.2_05
 
-call %ANT_HOME%\bin\ant.bat -f AntTest.xml
+REM Set "headless" to "true" to skip all GUI tests
+REM Set "noadmin" to "true" to skip all tests that require elevated rights
+call %ANT_HOME%\bin\ant.bat -f AntTest.xml -Dheadless=false -Dnoadmin=false
 
 PAUSE
 
