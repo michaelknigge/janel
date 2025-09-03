c258a8779e9fc14a143e6188ef23d06775c2498f
Michael Knigge <michael.knigge@set.de>
2025-09-03 15:24:17 +0200

    Changed path to devenv in .bat files (VS2017 -> VS2019)

--- Source/makecon.bat
+++ Source/makecon.bat
@@ -5,10 +5,10 @@ rem Change the JAVA_HOME environment variable to the directory of your JDK (*NOT
 rem set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_101
 
 echo Making the console executable
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.com" Janel.sln /rebuild "Release Console|Win32"
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.com" Janel.sln /rebuild "Release Console|Win32"
 copy ".\Release Console\Janel.exe" ..\Executables\Win32\JanelConsole32.exe
 
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.com" Janel.sln /rebuild "Release Console|x64"
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.com" Janel.sln /rebuild "Release Console|x64"
 copy ".\x64\Release Console\Janel.exe" ..\Executables\x64\JanelConsole64.exe
 
 @endlocal
--- Source/makewin.bat
+++ Source/makewin.bat
@@ -5,10 +5,10 @@ rem Change the JAVA_HOME environment variable to the directory of your JDK (*NOT
 rem set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_101
 
 echo Making the non-console executable
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.com" Janel.sln /rebuild "Release Windows|Win32"
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.com" Janel.sln /rebuild "Release Windows|Win32"
 copy ".\Release Windows\Janel.exe" ..\Executables\Win32\JanelWindows32.exe
 
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.com" Janel.sln /rebuild "Release Windows|x64"
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.com" Janel.sln /rebuild "Release Windows|x64"
 copy ".\x64\Release Windows\Janel.exe" ..\Executables\x64\JanelWindows64.exe
 
 @endlocal
